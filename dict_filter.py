b2917aa17cedac263b3f076a7e90fb15f4343e2f
Jack Park <75093072+jackpark071801@users.noreply.github.com>
2024-09-30 17:01:12 -0400

    English Dictionary Loading and Filtering
    Reading from the downloaded english dictionary json file and filtering by words that are of length 4 and greater because those are the only ones available to be used in Letter Boxed and then downloading the filtered json file to be used for the script that solves Letter Boxed

---
+++ dict_filter.py
@@ -0,0 +1,28 @@
+import json
+
+def load_words(json_file):
+    with open(json_file, 'r') as file:
+        words = json.load(file)
+    return words
+
+def filter_words(words):
+    return [word for word in words if len(word) > 4]
+
+def save_filtered_words(filtered_words, output_file):
+    with open(output_file, 'w') as file:
+        json.dump(filtered_words, file, indent=4)
+    print(f"Filtered words saved to {output_file}")
+
+def main():
+    json_file = 'words_dictionary.json'
+    
+    output_file = 'D:/Fun_Programs/Letter Boxed/english_words.json'
+    
+    words = load_words(json_file)
+    
+    filtered_words = filter_words(words)
+    
+    save_filtered_words(filtered_words, output_file)
+
+if __name__ == "__main__":
+    main()
